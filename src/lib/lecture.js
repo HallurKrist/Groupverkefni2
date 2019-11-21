e607869eac70e542363aead2b29659a67ab9b30c
Hallur Kristinn Hallsson <hkh32@hi.is>
2019-11-21 16:39:55 +0000

    lecture

--- src/lib/lecture.js
+++ src/lib/lecture.js
@@ -1,1 +1,34 @@
 // föll sem taka element og data sem viðfang og byr til hlut á elementið eftir því hvernig data það er.
+
+export function makeLecturePart(element, data) {
+
+}
+
+function makeYoutube() {
+
+}
+
+function makeText() {
+ 
+}
+
+function makeHeading() {
+ 
+}
+
+function makeList() {
+ 
+}
+
+function makeQuote() {
+ 
+}
+/*
+function make() {
+ 
+}
+
+function make() {
+ 
+}
+*/
